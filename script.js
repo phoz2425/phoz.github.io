dc83cbbe0601bde16bba13bb8edc4deb430cf9a0
phoz2425 <166633133+phoz2425@users.noreply.github.com>
2024-04-16 21:54:30 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    window.location.href = 'nextPage.html'; // Replace with the URL of the page you want to navigate to
+});
