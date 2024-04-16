4e45d1e62675b5de119d60f3916d73fe84b818d0
phoz2425 <166633133+phoz2425@users.noreply.github.com>
2024-04-17 01:50:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-var correctAnswers = 0;
-var level = 0;
-var item = 0;
-var charIndex = 0;
-var items = [];
-var currentChar;
-const levels = [
-  // Level 1
-  [
-    ['2', 'H'],
-    ['6', 'P']
-  ],
-  // Level 2
-  [
-    ['4', '7', 'O'],
-    ['3', '0', 'R']
-  ],
-  // Level 3
-  [
-    ['A', 'Z', '4', '7'],
-    ['P', 'J', '4', '8'],
-    ['L', 'J', '2', '3']
-  ],
-  // Level 4
-  [
-    ['L', 'B', 'J', '2', '3', '6'],
-    ['V', 'W', 'B', '9', '1', '8'],
-    ['K', 'B', 'R', '2', '4', '8']
-  ]
-];
-
 function startGame() {
     // Hide the start button
     document.getElementById('startButton').style.display = 'none';
@@ -49,27 +18,16 @@ function startGame() {
             document.getElementById('instructions').innerHTML = 'LETTER-NUMBER SEQUENCING<br>Enter the digits from smallest to largest, and then the letters in alphabetical order.';
             showTextbox();
         }, 1000);
-
-        // Move to the next character, item or level
-        if (charIndex < levels[level][item].length - 1) {
-            charIndex++;
-        } else if (item < levels[level].length - 1) {
-            item++;
-            charIndex = 0;
-        } else {
-            level++;
-            item = 0;
-            charIndex = 0;
-        }
     } else {
         // Show the number of correct answers
         document.querySelector('.container').textContent = 'Correct answers: ' + correctAnswers;
     }
 }
+
 function showTextbox() {
     var textbox = document.createElement('input');
     textbox.type = 'text';
-    textbox.maxLength = 6; // Limit input to 6 characters
+    textbox.maxLength = 1; // Limit input to 1 character
     textbox.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
             // Check the answer
@@ -81,6 +39,18 @@ function showTextbox() {
 
             // Remove the textbox
             document.querySelector('.container').removeChild(textbox);
+
+            // Move to the next character, item or level
+            if (charIndex < levels[level][item].length - 1) {
+                charIndex++;
+            } else if (item < levels[level].length - 1) {
+                item++;
+                charIndex = 0;
+            } else {
+                level++;
+                item = 0;
+                charIndex = 0;
+            }
 
             // Start the next round
             startGame();
