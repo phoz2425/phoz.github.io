eb425ace28b09d17776d32a9a2af3f4f8df17134
phoz2425 <166633133+phoz2425@users.noreply.github.com>
2024-04-17 23:15:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-var correctAnswers = 0;
-var level = 0;
-var item = 0;
-var charIndex = 0;
-var items = [];
-var currentChar;
-const levels = [
-  // Level 1
-  [
-    ['2', 'H'],
-    ['6', 'P']
-  ],
-  // Level 2
-  [
-    ['4', '7', 'O'],
-    ['3', '0', 'R']
-  ],
-  // Level 3
-  [
-    ['A', 'Z', '4', '7'],
-    ['P', 'J', '4', '8'],
-    ['L', 'J', '2', '3']
-  ],
-  // Level 4
-  [
-    ['L', 'B', 'J', '2', '3', '6'],
-    ['V', 'W', 'B', '9', '1', '8'],
-    ['K', 'B', 'R', '2', '4', '8']
-  ]
-];
+
 
 function startGame() {
     // Hide the start button
