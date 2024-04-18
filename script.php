336a21e5be33daee900fd14dd5791b636daa4a2c
phoz2425 <166633133+phoz2425@users.noreply.github.com>
2024-04-18 11:23:51 +0800

    Create script.php

---
+++ script.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM devices";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
