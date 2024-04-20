0b2aa9f0afc37d7a1f113236c21b937c557d8780
phoz2425 <166633133+phoz2425@users.noreply.github.com>
2024-04-21 03:47:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function checkAnswer(userAnswer, level, item) {
 
     if (userAnswer === correctAnswer) {
         console.log("Correct answer");
-        gameResults.correctAnswers.push(correctAnswer); // Add correct answer to gameResults
+        gameResults.correctAnswers.push({level: level, item: item, answer: userAnswer, correct: true}); // Add correct answer to gameResults
     } else {
         gameResults.wrongAnswers.push({level: level, item: item, answer: userAnswer, correct: false});
     }
